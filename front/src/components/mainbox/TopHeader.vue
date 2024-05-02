cd986abd06a16e68a90eed8cdf0140f450df7be1
lkwavestian <lkwavestian@gmail.com>
2024-05-02 21:36:36 +0800

    feat: TopHeader模块

--- front/src/components/mainbox/TopHeader.vue
+++ front/src/components/mainbox/TopHeader.vue
@@ -1,3 +1,48 @@
 <template>
-    <el-header>Header</el-header>
+    <el-header>
+        <div>学生学业质量分析系统</div>
+        <div>
+            <span style="line-height: 40px">欢迎{{ username }}回来</span>
+            <el-dropdown>
+                <!-- <el-avatar :size="40" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" /> -->
+                <el-avatar :size="40" :src="avatar"></el-avatar>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>{{ roleName }}</el-dropdown-item>
+                        <el-dropdown-item @click="handleExit">退出</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </div>
+    </el-header>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import useRouterStore from '../../store/useRouterStore';
+import useUserStore from '../../store/useUserStore';
+const router = useRouter();
+const { changeRouter } = useRouterStore();
+const {
+    changeUser,
+    user: {
+        role: { roleName },
+        username,
+    },
+} = useUserStore();
+const handleExit = () => {
+    changeUser({});
+    changeRouter(false);
+    router.push('/login');
+};
+</script>
+
+<style lang="scss" scoped>
+.el-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #0d47a1;
+    color: white;
+}
+</style>
--- front/src/views/Mainbox.vue
+++ front/src/views/Mainbox.vue
@@ -3,7 +3,6 @@
         <el-container style="height: 100vh">
             <SideMenu />
             <el-container direction="vertical">
-                <button @click="handleExit">退出</button>
                 <TopHeader />
                 <el-main>
                     <el-scrollbar>
@@ -16,19 +15,6 @@
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
-import useRouterStore from '../store/useRouterStore';
-import useUserStore from '../store/useUserStore';
 import SideMenu from '../components/mainbox/SideMenu.vue';
 import TopHeader from '../components/mainbox/TopHeader.vue';
-
-const router = useRouter();
-const { changeUser } = useUserStore();
-const { changeRouter } = useRouterStore();
-
-const handleExit = () => {
-    changeUser({});
-    changeRouter(false);
-    router.push('/login');
-};
 </script>
